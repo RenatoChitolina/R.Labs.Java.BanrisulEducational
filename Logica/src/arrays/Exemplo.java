5d712ecea46a710977ca49f4da2a8eb0e71a13e5
Renato Lucas Chitolina <renato.chitolina@hotmail.com>
2023-03-10 15:42:00 -0300

    Removing unnecessary comment blocks.

--- Logica/src/arrays/Exemplo.java
+++ Logica/src/arrays/Exemplo.java
@@ -5,7 +5,6 @@ public class Exemplo {
         // Array de 1 dimensão (Vetor)
         // Índices       0   1   2   3   4   5   6   7
         int[] vetor = { 14, 17, 26, 50, 69, 67, 86, 98 };
-        // System.out.println(vetor[0]);
         for (int indice = 0; indice < vetor.length; indice++) {
             System.out.println(vetor[indice]);
         }
@@ -13,8 +12,6 @@ public class Exemplo {
         // Array de 2 dimensões (Matriz)
         // Índices          0-0 0-1 0-2     1-0  1-1 1-2     2-0 2-1 2-2    2-3     3-0 3-1 3-2
         int[][] matriz = { { 67, 60, 79 }, { 78, 801, 45 }, { 73, 36, 78, 99999 }, { 45, 67, 88 } };
-        // System.out.println(matriz[0][0]);
-        // System.out.println(matriz.length);
         for (int linha = 0; linha < matriz.length; linha++) {
             // Estou iterando sobre linhas
             System.out.println("---------- linha " + linha + " ------------------------ ");
